9573864008a7aaf450577e7b92271a91b46f5478
Jbur43 <jtburum@gmail.com>
2023-06-14 17:19:58 -0400

    create if not exists

--- lib/kinesis_client/stream/app_state/ecto/migration.ex
+++ lib/kinesis_client/stream/app_state/ecto/migration.ex
@@ -6,7 +6,7 @@ defmodule KinesisClient.Stream.AppState.Ecto.Migration do
       "CREATE TABLE IF NOT EXISTS shard_lease (shard_id VARCHAR(255) PRIMARY KEY, checkpoint VARCHAR(255), lease_owner VARCHAR(255), lease_count INTEGER, completed BOOLEAN)"
     )
 
-    create(unique_index(:shard_lease, [:shard_id]))
+    create_if_not_exists(unique_index(:shard_lease, [:shard_id]))
   end
 
   def down do
