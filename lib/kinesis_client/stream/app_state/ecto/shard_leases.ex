886bcd2c6944005dabb10488060492c672f28511
Jon Kusumo <tahir.kusumo@paradox.ai>
2024-11-14 11:15:26 -0500

    update on specs

--- lib/kinesis_client/stream/app_state/ecto/shard_leases.ex
+++ lib/kinesis_client/stream/app_state/ecto/shard_leases.ex
@@ -3,7 +3,7 @@ defmodule KinesisClient.Stream.AppState.Ecto.ShardLeases do
 
   import Ecto.Query
 
-  @spec get_shard_lease(map, Ecto.Repo.t()) :: {:error, :not_found} | {:ok, struct()}
+  @spec get_shard_lease(map, Ecto.Repo.t()) :: {:error, :not_found} | {:ok, ShardLease.t()}
   def get_shard_lease(params, repo) do
     ShardLease.query()
     |> ShardLease.build_get_query(params)
@@ -14,21 +14,23 @@ defmodule KinesisClient.Stream.AppState.Ecto.ShardLeases do
     end
   end
 
-  @spec get_shard_lease_by_id(String.t(), Ecto.Repo.t()) :: {:error, :not_found} | {:ok, struct()}
+  @spec get_shard_lease_by_id(String.t(), Ecto.Repo.t()) ::
+          {:error, :not_found} | {:ok, ShardLease.t()}
   def get_shard_lease_by_id(shard_id, repo) do
     %{shard_id: shard_id}
     |> get_shard_lease(repo)
   end
 
-  @spec insert_shard_lease(map, Ecto.Repo.t()) :: {:error, Ecto.Changeset.t()} | {:ok, struct()}
+  @spec insert_shard_lease(map, Ecto.Repo.t()) ::
+          {:error, Ecto.Changeset.t()} | {:ok, ShardLease.t()}
   def insert_shard_lease(attrs, repo) do
     %ShardLease{}
     |> ShardLease.changeset(attrs)
     |> repo.insert()
   end
 
-  @spec update_shard_lease(struct(), Ecto.Repo.t(), list) ::
-          {:error, :update_unsuccessful} | {:ok, struct()}
+  @spec update_shard_lease(ShardLease.t(), Ecto.Repo.t(), list) ::
+          {:error, :update_unsuccessful} | {:ok, ShardLease.t()}
   def update_shard_lease(shard_lease, repo, change \\ []) do
     from(sl in ShardLease,
       where: sl.shard_id == ^shard_lease.shard_id and sl.lease_count == ^shard_lease.lease_count,
