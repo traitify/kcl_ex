aa2cfc616331de02758f410f5e369145bd58b9be
Jon Kusumo <tahir.kusumo@paradox.ai>
2024-11-14 08:57:19 -0500

    handle ecto lease count correctly

--- lib/kinesis_client/stream/app_state/ecto/shard_leases.ex
+++ lib/kinesis_client/stream/app_state/ecto/shard_leases.ex
@@ -1,7 +1,9 @@
 defmodule KinesisClient.Stream.AppState.Ecto.ShardLeases do
   alias KinesisClient.Stream.AppState.Ecto.ShardLease
 
-  @spec get_shard_lease(map, Ecto.Repo.t()) :: {:error, :not_found} | {:ok, ShardLease.t()}
+  import Ecto.Query
+
+  @spec get_shard_lease(map, Ecto.Repo.t()) :: {:error, :not_found} | {:ok, struct()}
   def get_shard_lease(params, repo) do
     ShardLease.query()
     |> ShardLease.build_get_query(params)
@@ -12,29 +14,31 @@ defmodule KinesisClient.Stream.AppState.Ecto.ShardLeases do
     end
   end
 
-  @spec get_shard_lease_by_id(String.t(), Ecto.Repo.t()) ::
-          {:error, :not_found} | {:ok, ShardLease.t()}
+  @spec get_shard_lease_by_id(String.t(), Ecto.Repo.t()) :: {:error, :not_found} | {:ok, struct()}
   def get_shard_lease_by_id(shard_id, repo) do
     %{shard_id: shard_id}
     |> get_shard_lease(repo)
   end
 
-  @spec insert_shard_lease(map, Ecto.Repo.t()) ::
-          {:error, Ecto.Changeset.t()} | {:ok, ShardLease.t()}
+  @spec insert_shard_lease(map, Ecto.Repo.t()) :: {:error, Ecto.Changeset.t()} | {:ok, struct()}
   def insert_shard_lease(attrs, repo) do
     %ShardLease{}
     |> ShardLease.changeset(attrs)
     |> repo.insert()
   end
 
-  @spec update_shard_lease(ShardLease.t(), Ecto.Repo.t(), list) ::
-          {:error, Ecto.Changeset.t()} | {:ok, ShardLease.t()}
+  @spec update_shard_lease(struct(), Ecto.Repo.t(), list) ::
+          {:error, :update_unsuccessful} | {:ok, struct()}
   def update_shard_lease(shard_lease, repo, change \\ []) do
-    shard_lease_changeset = Ecto.Changeset.change(shard_lease, change)
-
-    case repo.update(shard_lease_changeset) do
-      {:ok, shard_lease} -> {:ok, shard_lease}
-      {:error, changeset} -> {:error, changeset}
+    from(sl in ShardLease,
+      where: sl.shard_id == ^shard_lease.shard_id and sl.lease_count == ^shard_lease.lease_count,
+      select: sl,
+      update: [set: ^change]
+    )
+    |> repo.update_all([])
+    |> case do
+      {1, shard_lease} -> {:ok, shard_lease}
+      {0, _} -> {:error, :update_unsuccessful}
     end
   end
 end
