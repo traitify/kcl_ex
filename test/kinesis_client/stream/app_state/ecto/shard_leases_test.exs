5fa3b8f99742671a40a92763a281a05132200623
Jon Kusumo <tahir.kusumo@paradox.ai>
2025-01-17 09:15:25 -0500

    remove unnecessary test

--- test/kinesis_client/stream/app_state/ecto/shard_leases_test.exs
+++ test/kinesis_client/stream/app_state/ecto/shard_leases_test.exs
@@ -21,16 +21,6 @@ defmodule KinesisClient.Stream.AppState.Ecto.ShardLeasesTest do
     assert shard_lease.lease_owner == "test_owner"
   end
 
-  test "get_shard_lease_by_id/2" do
-    {:ok, [shard_lease]} = get_shard_lease_by_id("a.b.c", Repo)
-
-    assert shard_lease.shard_id == "a.b.c"
-    assert shard_lease.checkpoint == nil
-    assert shard_lease.completed == false
-    assert shard_lease.lease_count == 1
-    assert shard_lease.lease_owner == "test_owner"
-  end
-
   describe "insert_shard_lease/2" do
     test "returns a new shard_lease" do
       attrs = %{
